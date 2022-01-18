f18b5f4cbfd51d9621595057d37467af314ac12c
marcosantonastasi <marco.santonastasi@gmail.com>
2022-01-18 20:12:06 +0100

    starter_code done

---
+++ exercises/01-exe-js_obj_basics.js
@@ -0,0 +1,26 @@
+const ironhacker = {
+    firstName: 'Tio',
+    age: 39,
+    favoriteLanguage: 'JavaScript',
+    isSatisfied: true,
+    'works at': 'Google',
+    isRemote: false
+}
+
+// 1: update favoriteLanguage to 'React' 
+// ... your code here
+
+// 2: Update ironhacker's age to 29
+// ... your code here
+
+// 3: Remove the 'isRemote' property
+// ... your code here
+
+// 4: Add a new property: 'didGraduate' and set it to a valid boolean value
+// ... your code here
+
+// 5: list all keys of the ironhacker object
+// ... your code here
+
+// 6: check if ironhacker has property 'works at'
+// ... your code here
