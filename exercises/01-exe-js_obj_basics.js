96dbd016049bb724d0d8a4681c3ae12c5a21538b
Marco Santonastasi <84954908+marco-ih-bcn@users.noreply.github.com>
2022-04-08 13:38:55 +0200

    done exercise soulution in complete

--- exercises/01-exe-js_obj_basics.js
+++ exercises/01-exe-js_obj_basics.js
@@ -39,3 +39,55 @@ for(const key in ironhacker){
 // 6: check if ironhacker has property 'works at'
 // ... your code here
 console.log(ironhacker["worksAt"], ironhacker.hasOwnProperty("worksAt"), !!ironhacker.worksAt)
+
+
+// Use the following object for the remaining part 
+
+const student = {
+    firstName: 'Paola',
+    lastName: 'Sanchez',
+    bootcamp: 'Web Dev',
+    favorites: ['JavaScript', 'HTML'],
+    "is career changer": true
+  };
+  
+  console.log('student obj before modifications: ', student);
+  
+  // console.log the value of firstName
+  console.log(student.firstName); // => Paola
+  
+  // print "She is an Iornhacker!" if "is carreer changer" is true
+  if (student['is career changer'])console.log("She is an Iornhacker!");
+  
+  // if the obj has both the first and last name, print "Studnet profile complete!"
+  // CHECK DOES PROPERTY EXIST IN AN OBJECT
+  if('firstName' in student && 'lastName' in student) console.log("Studnet profile complete!");
+
+  // Add "year" property and set it to 2021
+  student.year = 2021;
+  
+  console.log('studnet obj after year has been added: ', student);
+  // => { firstName: 'Paola',
+  //      lastName: 'Sanchez',
+  //      bootcamp: 'Web Dev',
+  //      favorites: ['JavaScript', 'HTML'],
+  //      "is career changer": true,
+  //      year: 2018 }
+  
+  // Delete the "bootcamp" property
+  delete student.bootcamp;
+  
+  // Update the date to 2022
+  student.year = 2022;
+  
+  // List all the object keys
+  const objKeys = Object.keys(student);
+  console.log('objKeys => ', objKeys); // =>  [ 'firstName', 'lastName', 'favorites', 'isCareerChanger', 'year' ]
+  
+  // List all the object values
+  const objValues = Object.values(student);
+  console.log('objValues => ', objValues); // =>  [ 'Paola', 'Sanchez', [ 'JavaScript', 'HTML' ], true, 2019 ]
+  
+  for (let key in student) {
+    console.log(key);
+  }
